3d282939790eb863bcb0f3d842dda446eafd9a8c
gabrielmoraesjulio <golk76@outlook.com>
2023-07-16 15:09:13 -0300

    Views e Controllers de Alterar e Detalhes

---
+++ controllers/alterarController.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const router = express.Router()
+
+const produtosModel = require("../models/Produtos")
+
+router.post("/alterar", async (req, res) => {
+    const paginaNav = 1
+    
+    const detalheProduto = await produtosModel.find({
+        _id: req.body.idProduto
+    })
+
+    res.render("detalheProduto", {
+        detalheProdutoVar: detalheProduto,
+        paginaNav: paginaNav
+    })
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ require("dotenv").config()
 const homeController = require("./controllers/homeController")
 const produtosController = require("./controllers/produtosController")
 const adminController = require("./controllers/adminController")
+const detalhesController = require("./controllers/detalhesController")
+const alterarController = require("./controllers/alterarController")
 
 app.set('view engine', 'ejs')
 app.use(express.static("assets"))
@@ -16,6 +18,8 @@ app.use(bodyParser.json())
 app.get("/", homeController)
 app.post("/novoproduto", produtosController)
 app.get("/admin", adminController)
+app.post("/details", detalhesController)
+app.post("/alterar", alterarController)
 
 const port = process.env.PORT
 app.listen(port || 4040, console.log(`Server running in http://localhost:${port}`))
